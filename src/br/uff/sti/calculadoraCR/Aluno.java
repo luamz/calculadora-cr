6d4c0a60885201b977666f911128f7c745adc542
Luam Silva <luamsilva@id.uff.br>
2020-11-25 16:58:13 -0200

    Commit inicial

--- src/br/uff/sti/calculadoraCR/Aluno.java
+++ src/br/uff/sti/calculadoraCR/Aluno.java
@@ -1,14 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.uff.sti.calculadoraCR;
 
-/**
- *
- * @author Luam
- */
+import java.util.ArrayList;
+
+/* @author Luam */
+
 public class Aluno {
+    String matricula;
+    String codCurso;
+    ArrayList<Disciplina> historico =  new ArrayList();
+    int totalCargaHoraria;
+    
     
 }
--- src/br/uff/sti/calculadoraCR/Disciplina.java
+++ src/br/uff/sti/calculadoraCR/Disciplina.java
@@ -1,14 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.uff.sti.calculadoraCR;
 
-/**
- *
- * @author Luam
- */
+/* @author Luam  */
 public class Disciplina {
+    String codCurso;
+    int nota;
+    int cargaHoraria;
+    int anoSemestre;
     
 }
